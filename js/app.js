b449fa19bf06453d0220ac227e693023ab49072c
Guillaume97490 <techer.guillaume97490@gmail.com>
2019-04-17 23:36:15 +0400

    Switch Pc/Mobile pour image, logos agile et google

--- js/app.js
+++ js/app.js
@@ -1,19 +1,32 @@
 new Vue({
     el: "#app",
     data: {
+        changeImgTuto:"",
+        changeImgChat:"",
+        changeImgToDoList:"",
+        changeImgLandingPage:"",
 
     },
     methods: {
         closeMenu(){
             
-            
-
             document.getElementById("checkboxMenu").click(); // Click on the checkbox
-            
+        },
 
 
+        toogleImgTuto(){
+            this.changeImgTuto = !this.changeImgTuto
+        },
 
-        }
+        toogleImgChat(){
+            this.changeImgChat = !this.changeImgChat
+        },
+        toogleImgToDoList(){
+            this.changeImgToDoList = !this.changeImgToDoList
+        },
+        toogleImgLandingPage(){
+            this.changeImgLandingPage = !this.changeImgLandingPage
+        },
 
-    }
+    },
 })
